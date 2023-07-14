52beeb3608104ee7aab3901e897bcc9017d8add5
dadyilhawallacevidal <wallace.vidal@dadyilha.com.br>
2023-07-14 15:36:20 -0300

    SetState #01

---
+++ src/views/examples/UseCallbackButtons.jsx
@@ -0,0 +1,23 @@
+import React, {memo} from "react";
+
+const UseCallbackButtons = (props) => {
+    console.log("render...");
+    return (
+        <div>
+            <button className="btn"
+                onClick={(e) => props.inc(parseInt(e.target.outerText))}>
+                +6
+            </button>
+            <button className="btn"
+                onClick={(e) => props.inc(parseInt(e.target.outerText))}>
+                +12
+            </button>
+            <button className="btn"
+                onClick={(e) => props.inc(parseInt(e.target.outerText))}>
+                +18
+            </button>
+        </div>
+    );
+}
+
+export default React.memo(UseCallbackButtons);
